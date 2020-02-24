e5e0af61e994e2b6886ec8a62dc08d5ec638fc97
kfettich <kfettich@gmail.com>
2020-02-24 14:45:25 -0500

    created CV using Nick Strayer's template

--- common_variables.R
+++ common_variables.R
@@ -1,20 +1,21 @@
 # Common non-csv data between CV and resume
 
 # Header with description of what is trying to be accomplished with this CV/Resume
-intro_text <- "I have made [visualizations viewed by hundreds of thousands of people](https://www.nytimes.com/interactive/2016/08/26/us/college-student-migration.html), [sped up query times for 25 terabytes of data by an average of 4,800 times](https://livefreeordichotomize.com/2019/06/04/using_awk_and_r_to_parse_25tb/), and built [packages for R](https://github.com/nstrayer/shinysense) that let you [do magic](http://nickstrayer.me/dataDayTexas/).
+intro_text <- "I am enthusiastic about using my skills, expertise and time to make a lasting positive impact in the community. Professionally, my work seeks to improve healthcare for aging 
+individuals, and has [earned an Excellence in Technology Award for Dignity in Long-Term Care](https://www.mcknights.com/news/dunwoody-named-dignity-gold-award-winner/). Personally, I coordinate
+efforts to [increase diversity in the Philadelphia data science community](https://www.meetup.com/rladies-philly/), [reduce the number of homeless animals in the city](https://github.com/rladiesPHL/2019_datathon/) 
+and [facilitate care and recovery for those experiencing opioid addiction](https://github.com/CodeForPhilly/datahack2020)."
 
-Currently searching for a position that allows me to build tools leveraging a combination of visualization, machine learning, and software engineering to help people explore and understand their data in new and useful ways. 
-"
 
-
-# Language skills for skill bars visualization
+# Programming skills for skill bars visualization
 skills <- tribble(
   ~skill,               ~level,
   "R",                  5,
-  "Javascript (d3.js)", 4.5,
-  "C++",                4,
-  "Python",             4,
-  "Bash",               3.5,
-  "SQL",                3,
-  "AWK",                3
+  "RMarkdown",          5,
+  "SPSS",               5,
+  "Shiny Dashboards",   4,
+  "Python",             2,
+  "NoSQL",              2,
+  "SQL",                2,
+  "Jupyter Notebooks",  1
 )
